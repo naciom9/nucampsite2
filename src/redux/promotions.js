d227c049e527071851ded89dda3c4d84bb2b4f50
Ignace Mba <naciom9@yahoo.com>
2021-02-20 00:26:05 -0600

    Fetch API

--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,7 +1,19 @@
-import { PROMOTIONS } from '../shared/promotions'
+import { actions, actionTypes } from 'react-redux-form';
+import * as ActionTypes from './ActionTypes';
 
-export const Promotions = (state = PROMOTIONS, action) => {
+export const Promotions = (state = { isLoading: true,
+                                        errMess:null,
+                                        promotions: [] }, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_PROMOTIONS:
+            return {...state, isLoading: false, errMess: null, promotions: action.payload};
+
+        case action.PROMOTIONS_LOADING:
+            return {...state, isLoading: true, errMess: null, promotions: []};
+
+        case actionTypes.PROMOTIONS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
+            
         default:
             return state;
     }
