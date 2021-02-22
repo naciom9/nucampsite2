25ff634958c65de55b9958eeb0b7e99d0e7d7326
Ignace Mba <naciom9@yahoo.com>
2021-02-21 22:47:14 -0600

    updated to Fetch Comments through the json server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -24,7 +24,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -38,7 +38,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         </div>
                     );
                 })}
-                 <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                 <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -66,7 +66,7 @@ class CommentForm extends Component {
     
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
     
         
@@ -168,7 +168,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
